c06ae4a9ab331f4d22eec970bf3a1cd883098252
beshoyalber <59097861+beshoyalber@users.noreply.github.com>
2021-05-23 20:23:16 +0200

    Update main.py
    Reads specific n bytes from Matlab.

--- main.py
+++ main.py
@@ -22,3 +22,19 @@ def readnbyte(sock, n):
         cr = sock.recv_into(memoryview(buff)[pos:])
         pos += cr
     return buff
+
+#Reconstructing n bytes received to images
+def read_TCP_image(data):
+    imagenumpy = np.array(data,dtype = np.uint8)
+    R1 = imagenumpy[0:Res].reshape((H,W))
+    G1 = imagenumpy[Res:Res*2].reshape((H,W))
+    B1 = imagenumpy[Res*2:Res*3].reshape((H,W))
+    #R2 = imagenumpy[Res*3:Res*4].reshape((H,W))
+    #G2 = imagenumpy[Res*4:Res*5].reshape((H,W))
+    #B2 = imagenumpy[Res*5:Res*6].reshape((H,W))
+    imgL = np.dstack((R1,G1,B1))
+    #imgR = np.dstack((R2,G2,B2))
+    imgL = np.rot90(imgL, 3)
+    #imgR = np.rot90(imgR, 3)
+    #return imgL,imgR
+    return imgL
