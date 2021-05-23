5f8bd2cfd980c26bbe1ae2e37085ed4019a9ebda
beshoyalber <59097861+beshoyalber@users.noreply.github.com>
2021-05-23 20:40:24 +0200

    Update main.py
    Added receiving images and saving them.

--- main.py
+++ main.py
@@ -32,3 +32,33 @@ def read_TCP_image(data):
     imgL = np.dstack((R1,G1,B1))
     imgL = np.rot90(imgL, 3)
     return imgL
+
+#Reads images and saves them or shows them
+def get_frames_four_sides(path,sizeimg,num_frames = -1,port = 1117,host = "127.0.0.1",save_img = True , show_img = False):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        conn, addr = s.accept()
+        iterator2 = 0
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = readnbyte(conn,sizeimg*4)
+                if (not data):
+                    cv2.destroyAllWindows()
+                    break
+                imgL = read_TCP_image(data[0:sizeimg])
+                imgR = read_TCP_image(data[sizeimg:sizeimg*2])
+                imgT = read_TCP_image(data[sizeimg*2:sizeimg*3])
+                imgB = read_TCP_image(data[sizeimg*3:sizeimg*4])
+                if(save_img):
+                    cv2.imwrite(os.path.join(path,'left/frame'+str(iterator2)+'.png'),imgL)
+                    cv2.imwrite(os.path.join(path,'right/frame'+str(iterator2)+'.png'),imgR)
+                    cv2.imwrite(os.path.join(path,'front/frame'+str(iterator2)+'.png'),imgT)
+                    cv2.imwrite(os.path.join(path,'back/frame'+str(iterator2)+'.png'),imgB)
+                if(show_img): 
+                    cv2.imshow('frame',imgL)
+                    cv2.waitKey(1)
+                iterator2+= 1
+                if (iterator2 == num_frames):
+                    break
